3ec70104cf97c088668e1a4da4d681632ee89d99
zer0pluss <zero.plluss@gmail.com>
2026-09-09 19:08:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -652,7 +652,7 @@ with st.sidebar:
 
     st.markdown(
         '<div class="side-label">القائمة الرئيسية</div>',
-        unsafe_allow_html=True
+        unsafe_allow_html=True,
         text-align:right;
     )
 
