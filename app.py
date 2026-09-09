16948d9e462a085ac02350e8b7be37b29b91e091
zer0pluss <zero.plluss@gmail.com>
2026-09-09 17:28:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ def set_custom_design():
         .stApp::before {{
             content: "";
             position: fixed;
-            inset: 0;
+            inset: 1;
             background-image: url("data:image/jpeg;base64,{logo_b64}");
             background-repeat: repeat;
             background-position: 80% 70%;
