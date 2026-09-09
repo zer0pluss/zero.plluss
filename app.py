b391ba1eb2408f8a3a0ed75f01b5533cbc743a5c
zer0pluss <zero.plluss@gmail.com>
2026-09-09 19:31:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -808,14 +808,14 @@ if choice == "➕  تسجيل طلب جديد":
                 "التكلفة الإجمالية (جنيه)",
                 min_value=0.0,
                 step=0.0,
-                 format=""
+                 format="%."
             )
 
             deposit = st.number_input(
                 "المبلغ المدفوع / العربون",
                 min_value=0.0,
                 step=0.0,
-                format=""
+                format="%."
             )
 
             order_status = st.selectbox(
