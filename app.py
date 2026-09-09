78b69c5c1fe522bb4f66940d85d4f459933aec1b
zer0pluss <zero.plluss@gmail.com>
2026-09-09 17:46:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -108,7 +108,7 @@ def set_custom_design():
             inset: 0;
             background-image: url("data:image/jpeg;base64,{logo_b64}");
             background-repeat: non-repeat;
-            background-position: 20% 20%;
+            background-position: 20% 30%;
             background-size: min(1500px, 85vw);
             opacity: 0.077;
             filter: grayscale(0%) contrast(120%);
